8fb385c8af001fd19a78aba1ae975f580520d298
Chinh Le <chinh.ln1902@gmail.com>
2026-09-23 13:37:17 -0700

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        HashMap<Character, Integer> s1Count = new HashMap<>();
+        HashMap<Character, Integer> s2Window = new HashMap<>();
+
+        int left = 0;
+
+        for (int i = 0; i < s1.length(); i++) {
+            s1Count.put(s1.charAt(i), s1Count.getOrDefault(s1.charAt(i), 0) + 1);
+        }
+
+        for (int right = 0; right < s2.length(); right++) {
+
+            while (right - left + 1 > s1.length()) {
+                s2Window.put(s2.charAt(left), s2Window.getOrDefault(s2.charAt(left), 0) - 1);
+                if (s2Window.get(s2.charAt(left)) == 0) {
+                    s2Window.remove(s2.charAt(left));
+                }
+                left++;
+            }
+            s2Window.put(s2.charAt(right), s2Window.getOrDefault(s2.charAt(right), 0) + 1);
+
+            if (s2Window.equals(s1Count)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
